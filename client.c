8a790846cfd76828b2267bd87a9e89aa3b04f09e
rapha4x <rapha4.lx@gmail.com>
2024-11-30 03:51:41 -0300

    fixed norm

--- client.c
+++ client.c
@@ -1,34 +1,33 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
-
 #include "aux_func.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void    send_bits(const char c, int pid)
+void	send_bits(const char c, int pid)
 {
-    int		bits_count;
-	unsigned char bits;
+	int				bits_count;
+	unsigned char	bits;
 
 	bits = c;
-    bits_count = 8;
+	bits_count = 8;
 	while (bits_count--)
-    {
+	{
 		if (((bits >> bits_count) & 1))
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(1);
-    }
+		usleep(200);
+	}
 }
 
-void    send_message(const char *str, int pid)
+void	send_message(const char *str, int pid)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
+	i = 0;
+	while (str[i])
 	{
-        send_bits(str[i], pid);
+		send_bits(str[i], pid);
 		i++;
 	}
 }
@@ -39,15 +38,15 @@ void	error_message(const char *str)
 	exit(0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int server_pid;
+	int	server_pid;
 
 	if (argc <= 1)
 		error_message("You dont insert your PID\n");
 	else if (argc <= 2)
 		error_message("You dont insert your message\n");
-    server_pid = ft_atoi(argv[1]);
+	server_pid = ft_atoi(argv[1]);
 	if (server_pid <= 0)
 		error_message("PID 0 or less is not valid\n");
 	send_message(argv[2], server_pid);
