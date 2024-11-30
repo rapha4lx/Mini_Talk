055325ba341dabe57075959b8f0e0914f5ce3482
rapha4x <rapha4.lx@gmail.com>
2024-11-30 03:51:12 -0300

    fixed norm in aux

--- aux_func.h
+++ aux_func.h
@@ -1,7 +1,24 @@
-void    ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aux_func.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rferro-d <rferro-d@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 01:21:22 by rferro-d          #+#    #+#             */
+/*   Updated: 2024/11/30 01:26:47 by rferro-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void    ft_putstr(const char *str);
+#ifndef AUX_FUNC_H
+# define AUX_FUNC_H
 
-int     ft_atoi(const char *nptr);
+void	ft_putchar(char c);
+
+void	ft_putstr(const char *str);
+
+int		ft_atoi(const char *nptr);
 
 void	ft_putnbr(int n);
+
+#endif
